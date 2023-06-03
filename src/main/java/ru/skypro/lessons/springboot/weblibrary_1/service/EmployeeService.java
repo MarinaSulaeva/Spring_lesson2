1e70524c33dce6df803e45048d3a344b00050179
Марина Сулаева <sulaevamv@gmail.com>
2023-06-03 22:50:23 +0600

    Выполнила ДЗ по теме "Spring WEB", изменила тип коллекции для данных и скорректировала методы прошлого ДЗ.

--- src/main/java/ru/skypro/lessons/springboot/weblibrary_1/service/EmployeeService.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary_1/service/EmployeeService.java
@@ -1,14 +1,19 @@
 package ru.skypro.lessons.springboot.weblibrary_1.service;
-
 import ru.skypro.lessons.springboot.weblibrary_1.pojo.Employee;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.Map;
 import java.util.Optional;
 
 public interface EmployeeService {
-    List<Employee> getAllEmployees();
+    Map<Integer, Employee> getAllEmployees();
     public int getSumSalary();
     public Optional<Employee> getMaxSalary();
     public Optional<Employee> getMinSalary();
-    public List<Employee> getEmployeeWithSalaryAboveAverage();
+    public Map<Integer, Employee> getEmployeeWithSalaryAboveAverage();
+    public Employee getEmployeeById(Integer id) throws IOException;
+    public Map<Integer, Employee> getEmployeesWithSalaryHigherThan(Integer compareSalary);
+    public void deleteEmployeeById(Integer id) throws IOException;
+    public void editEmployee(Employee employee) throws IOException;
+    public void addEmployee(Employee employee) throws IOException;
 }
--- src/main/java/ru/skypro/lessons/springboot/weblibrary_1/service/EmployeeServiceImpl.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary_1/service/EmployeeServiceImpl.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Service;
 import ru.skypro.lessons.springboot.weblibrary_1.pojo.Employee;
 import ru.skypro.lessons.springboot.weblibrary_1.repository.EmployeeRepository;
 
+import java.io.IOException;
 import java.util.Comparator;
-import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -19,29 +20,60 @@ public class EmployeeServiceImpl implements EmployeeService{
     }
 
     @Override
-    public List<Employee> getAllEmployees() {
+    public Map<Integer, Employee> getAllEmployees() {
         return employeeRepository.getAllEmployees();
     }
 
     @Override
     public int getSumSalary() {
-        return getAllEmployees().stream().flatMapToInt(employee -> IntStream.of(employee.getSalary())).sum();
+        return getAllEmployees().values().stream().
+                flatMapToInt(employee -> IntStream.of(employee.getSalary())).sum();
     }
 
     @Override
     public Optional<Employee> getMaxSalary() {
-        return getAllEmployees().stream()
+        return getAllEmployees().values().stream()
                 .max(Comparator.comparingDouble(Employee::getSalary));
     }
 
     @Override
     public Optional<Employee> getMinSalary() {
-        return getAllEmployees().stream()
+        return getAllEmployees().values().stream()
                 .min(Comparator.comparingDouble(Employee::getSalary));
     }
 
     @Override
-    public List<Employee> getEmployeeWithSalaryAboveAverage() {
-        return getAllEmployees().stream().sorted(Comparator.comparingDouble(Employee::getSalary)).filter(employee -> employee.getSalary()>getSumSalary()/getAllEmployees().size()).collect(Collectors.toList());
+    public Map<Integer, Employee> getEmployeeWithSalaryAboveAverage() {
+        int i = getSumSalary()/getAllEmployees().size();
+        return getAllEmployees().entrySet().stream()
+                .filter(entry -> entry.getValue().getSalary()>i)
+                .collect(Collectors.toMap(Map.Entry :: getKey, Map.Entry :: getValue));
+    }
+
+    @Override
+    public Employee getEmployeeById(Integer id) throws IOException {
+        return employeeRepository.getEmployeeById(id);
+    }
+
+    @Override
+    public Map<Integer, Employee> getEmployeesWithSalaryHigherThan(Integer compareSalary) {
+        return getAllEmployees().entrySet().stream().
+                filter(entry -> entry.getValue().getSalary()>compareSalary).
+                collect(Collectors.toMap(Map.Entry :: getKey, Map.Entry :: getValue));
+    }
+
+    @Override
+    public void deleteEmployeeById(Integer id) throws IOException {
+        employeeRepository.deleteEmployeeById(id);
+    }
+
+    @Override
+    public void editEmployee(Employee employee) throws IOException {
+        employeeRepository.editEmployee(employee);
+    }
+
+    @Override
+    public void addEmployee(Employee employee) throws IOException {
+        employeeRepository.addEmployee(employee);
     }
 }
