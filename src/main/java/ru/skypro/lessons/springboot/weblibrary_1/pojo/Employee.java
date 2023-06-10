a74308341ecfe0370995d7fe0b05e0d4581e26ba
Марина Сулаева <sulaevamv@gmail.com>
2023-06-10 21:00:13 +0600

    методы getTheHighestSalary() не работает (поменяла fetch стратегию, getpage и getid сработали), добавила application.properties

--- src/main/java/ru/skypro/lessons/springboot/weblibrary_1/pojo/Employee.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary_1/pojo/Employee.java
@@ -20,7 +20,7 @@ public class Employee {
 
     private String name;
     private int salary;
-    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     @JoinColumn(name = "position_id")
     private Position position;
 
