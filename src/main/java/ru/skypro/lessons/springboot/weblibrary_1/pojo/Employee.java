96bd7e69a432b39ce675699d7cbd66650abdc5cc
Марина Сулаева <sulaevamv@gmail.com>
2023-06-10 14:56:23 +0600

    методы getTheHighestSalary(), getEmployeeById,getEmployeeWithPaging не работают

--- src/main/java/ru/skypro/lessons/springboot/weblibrary_1/pojo/Employee.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary_1/pojo/Employee.java
@@ -1,15 +1,28 @@
 package ru.skypro.lessons.springboot.weblibrary_1.pojo;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
+@Entity
+@Table(name = "employee")
+
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private int salary;
-
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn(name = "position_id")
+    private Position position;
 
 
 }
