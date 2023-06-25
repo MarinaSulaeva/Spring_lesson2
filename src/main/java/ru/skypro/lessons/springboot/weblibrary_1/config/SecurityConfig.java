4ec9bd89ce02a750757e5225005a7995c17e9909
Марина Сулаева <sulaevamv@gmail.com>
2023-06-25 20:41:57 +0600

    ДЗ по теме "Spring Security"

--- src/main/java/ru/skypro/lessons/springboot/weblibrary_1/config/SecurityConfig.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary_1/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.ignoringRequestMatchers("/*"))
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/**"))
                 .authorizeHttpRequests(this::customizeRequest)
                 .formLogin(Customizer.withDefaults())
                 .logout(Customizer.withDefaults());
